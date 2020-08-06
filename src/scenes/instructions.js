a86a0fe5da65bc1a42820f2865d39a6febac6457
Luis Saavedra <nriqu322@gmail.com>
2020-08-05 18:19:31 -0600

    style loading bar

--- src/scenes/instructions.js
+++ src/scenes/instructions.js
@@ -4,6 +4,12 @@ class Instructions extends Phaser.Scene {
   constructor() {
     super('Instructions');
   }
+
+  init(data) {}
+
+  create() {
+    this.add.image(300, 250, 'background');
+  }
 }
 
 export default Instructions;
--- src/scenes/loadScene.js
+++ src/scenes/loadScene.js
@@ -25,14 +25,48 @@ class LoadScene extends Phaser.Scene {
     this.load.image('ninjaIcon', ninjaIcon);
     // this.load.atlas('ninja', ninja, ninjaJson);
 
-    const loadingBar = this.add.graphics({
-      fillStyle: {
-        color: 0xffffff,
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(270, 300, 320, 50);
+
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+
+    const loadingText = this.make.text({
+      x: width / 2 - 10,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
       },
     });
+    loadingText.setOrigin(0.5, 0.5);
 
-    this.load.on('progress', (percent) => {
-      loadingBar.fillRect(0, this.game.renderer.height / 2, this.game.renderer.width * percent, 50);
+    const percentText = this.make.text({
+      x: width / 2 - 20,
+      y: height / 2 + 25,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    // const loadingBar = this.add.graphics({
+    //   fillStyle: {
+    //     color: 0xffffff,
+    //   },
+    // });
+
+    this.load.on('progress', (value) => {
+      percentText.setText(`${parseInt(value * 100, 10)}%`);
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(280, 310, 300 * value, 30);
+      // loadingBar.fillRect(0, this.game.renderer.height / 2, this.game.renderer.width * percent, 50);
     });
 
     this.load.on('complete', () => {
